f366d0695f885be2005e80d01ed8a15a0ff0212b
jader_tao <Jader_tao@qq.com>
2020-06-03 22:47:48 +0800

    feat(es): compose

---
+++ es/compose.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.compose = void 0;
+exports.compose = function () {
+    var fns = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        fns[_i] = arguments[_i];
+    }
+    return fns.reduce(function (f, g) { return function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        return f(g.apply(void 0, args));
+    }; });
+};
--- es/compose.ts
+++ es/compose.ts
@@ -1,1 +1,2 @@
-export const compose = (...fns: Function[]) => fns.reduce((f, g) => (...args: any[]) => f(g(...args)))
+type fn = (...args: any[]) => any
+export const compose = (...fns: fn[]) => fns.reduce((f, g) => (...args: any[]) => f(g(...args)))
